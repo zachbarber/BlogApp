d4f2495935fd9378803ba2501088c31251bb3860
Zach Barber <zachbarber321@gmail.com>
2021-01-04 00:26:36 -0500

    add Notes.txt file

--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
 import express from 'express';
 import path from 'path';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
 
-const PORT = process.env.PORT || 35565;
+const PORT = process.env.PORT;
+
+app.use(cors());
+
+app.use(bodyParser.json());
 
 app.listen(PORT, () => {
     console.log(`listening on ${PORT}..`);
 });
 
-app.get('/', (req, res) => {
- res.sendFile(path.join(__dirname,'./home.html'));
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, './home.html'));
+});
+
+app.post('/post', (req, res) => {
+    res.send('post received');
+});
+
+app.delete('/delete', (req, res) => {
+    res.send('delete received');
 });
