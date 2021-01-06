9b0f373ee1bff775735f4c3d4130e506a713c920
Dylan Barber <dylanbarber1@outlook.com>
2021-01-06 17:07:46 -0500

    Add get all method extension on service

--- server/src/services/BlogPostService.js
+++ server/src/services/BlogPostService.js
@@ -25,14 +25,23 @@ export class BlogPostService {
     }
 
     read(blogId) {
-        if(blogId) {
-        return new Promise((resolve, reject) => {
-            this.connection.query('SELECT * FROM blogs WHERE id = ?', [blogId], (err, rows) =>{
-                if (err){
-                    return reject(err);
-                }
-                resolve(rows);
+        if (blogId) {
+            return new Promise((resolve, reject) => {
+                this.connection.query('SELECT * FROM blogs WHERE id = ?', [blogId], (err, rows) => {
+                    if (err) {
+                        return reject(err);
+                    }
+                    resolve(rows);
+                });
             });
+        } else {
+            return new Promise((resolve, reject) => {
+                this.connection.query('SELECT * FROM blogs', (err, rows) => {
+                    if (err) {
+                        return reject(err);
+                    }
+                    resolve(rows);
+                });
             });
         }
 
