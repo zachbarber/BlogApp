9ef7b7236c3d2a5fb9759c8b65135ca1cfcc1985
Zach Barber <zachbarber321@gmail.com>
2021-01-06 21:54:12 -0500

    work on update method

--- server/src/services/BlogPostService.js
+++ server/src/services/BlogPostService.js
@@ -1,8 +1,11 @@
+import { resolve } from 'path';
+
 const mysql = require('mysql');
 
 export class BlogPostService {
 
     constructor() {
+
         this.connection = mysql.createConnection({
             host: 'localhost',
             user: 'root',
@@ -22,17 +25,30 @@ export class BlogPostService {
 
         this.validate(blogPost);
 
-        return new Promise((resolve, reject) => {
+        const createPost = new Promise((resolve, reject) => {
             this.connection.query('INSERT INTO blogs (title, body, created_at) VALUES (?, ?, NOW())', [blogPost.title, blogPost.body], (err, rows) => {
                 if (err) {
                     return reject(err);
                 }
+
                 resolve(rows);
             });
         });
+
+        createPost.then((data) => {
+            this.connection.query('SELECT * FROM blogs WHERE id = ?', [data.insertId], (err, rows) => {
+                if (err) {
+                    return reject(err);
+                }
+
+                resolve(rows);
+            });
+        })
+
     }
 
     read(blogId) {
+
         if (blogId) {
             return new Promise((resolve, reject) => {
                 this.connection.query('SELECT * FROM blogs WHERE id = ?', [blogId], (err, rows) => {
@@ -56,20 +72,22 @@ export class BlogPostService {
     }
 
     async update(blogPost) {
+
         if (typeof blogPost.id === 'number' && this.validate(blogPost) && await this.read(blogPost.id).length !== 0) {
-            
+            await this.connection.query('UPDATE blogs SET body = ?, modified_at = NOW() WHERE id = ?', [blogPost.body, blogPost.id]);
+
             return new Promise((resolve, reject) => {
-                this.connection.query('UPDATE blogs SET body = ? WHERE id = ?', [blogPost.body, blogPost.id], (err, rows) => {
+                this.connection.query('SELECT * FROM blogs WHERE id = ?', [blogPost.id], (err, rows) => {
                     if (err) {
                         return reject(err);
                     }
                     resolve(rows);
                 });
-            });
+            })
+        };
 
-        }
-        console.log(blogPost);
         throw new Error('Id must be supplied on update');
+
     }
 
     delete() {
