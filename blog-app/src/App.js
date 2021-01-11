2c7c049c2d4aabb660e51231e1ff4801daf28f9c
Zach Barber <zachbarber321@gmail.com>
2021-01-10 22:34:10 -0500

    add in BrowserRouter

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,14 +1,21 @@
 import BlogPosts from './components/BlogPosts';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 
 function App() {
   return (
 
-<div className='appContainer'>
-    <div className='contentContainer'>
-        <BlogPosts />
+    <div className='appContainer'>
+      <div className='contentContainer'>
+        <Router>
+          <BlogPosts />
+        </Router>
+      </div>
     </div>
-</div>
 
   );
 }
